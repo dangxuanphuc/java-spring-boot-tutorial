b70f2176e5943f40a16791b1a28fec01fda137b3
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 22:58:31 +0700

    Switch statement

--- Java/Demo.java
+++ Java/Demo.java
@@ -5,7 +5,33 @@ class Demo {
     int a = 6;
     String result = "";
 
-    result = a % 2 == 0 ? "Even" : "Odd";
-    System.out.println(result); // Even
+    switch (a) {
+      case 1:
+        result = "Monday";
+        break;
+      case 2:
+        result = "Tueday";
+        break;
+      case 3:
+        result = "Wednesday";
+        break;
+      case 4:
+        result = "Thursday";
+        break;
+      case 5:
+        result = "Friday";
+        break;
+      case 6:
+        result = "Saturday";
+        break;
+      case 7:
+        result = "Sunday";
+        break;
+      default:
+        result = "Enter a valid number";
+        break;
+    }
+
+    System.out.println(result); // Saturday
   }
 }
