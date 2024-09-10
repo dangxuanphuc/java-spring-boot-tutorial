3890430a2782209b2c7556a02eb89a7ee44d996c
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 21:35:25 +0700

    Arithmetic operators

--- Java/Hello.java
+++ Java/Hello.java
@@ -2,9 +2,10 @@ package Java;
 
 class Hello {
   public static void main(String args[]) {
-    byte a = 10;
-    byte b = 20;
-    int result = a * b;
-    System.out.println(result); // 200
+    int num = 0;
+    System.out.println(num++); // 0
+
+    num = 0;
+    System.out.println(++num); // 1
   }
 }
