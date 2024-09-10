f6271cf581f88c0ee9838e94a780e56109c66be7
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 22:22:10 +0700

    If / else

--- Java/Hello.java
+++ Java/Hello.java
@@ -3,11 +3,13 @@ package Java;
 class Hello {
   public static void main(String args[]) {
     int a = 6;
-    int b = 8;
-    int x = 5;
-    int y = 7;
 
-    boolean result = a < b && x > y;
-    System.out.println(result); // false
+    if (a > 0) {
+      System.out.println("a is positive number");
+    } else {
+      System.out.println("a is negative number or zero");
+    }
+
+    // a is positive number
   }
 }
