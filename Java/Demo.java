2ca3efae5c7ddd5a8e4f1108875bfe82f8bced8d
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 23:55:42 +0700

    Class and Object Practical

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,9 +1,19 @@
 package Java;
 
+class Calculator {
+  public int add(int num1, int num2) {
+    return num1 + num2;
+  }
+}
+
 class Demo {
   public static void main(String args[]) {
-    for (int i = 0; i <= 5; i++) {
-      System.out.println("Hi " + i);
-    }
+    int a = 4;
+    int b = 7;
+
+    Calculator calc = new Calculator();
+    int result = calc.add(a, b);
+
+    System.out.println(result);
   }
 }
