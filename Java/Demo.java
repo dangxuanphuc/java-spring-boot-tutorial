6a9bc09f706f86f4b35aa2fcc3736b1adbb4c9bf
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 22:36:29 +0700

    Ternary

---
+++ Java/Demo.java
@@ -0,0 +1,11 @@
+package Java;
+
+class Demo {
+  public static void main(String args[]) {
+    int a = 6;
+    String result = "";
+
+    result = a % 2 == 0 ? "Even" : "Odd";
+    System.out.println(result); // Even
+  }
+}
