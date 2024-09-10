013f05cf7ab5fcc131ffd98b937b46e8d783a6e6
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-10 21:31:53 +0700

    Literal

--- Java/Hello.java
+++ Java/Hello.java
@@ -2,16 +2,8 @@ package Java;
 
 class Hello {
   public static void main(String args[]) {
-    int num1 = 3;
-    byte by = 127;
-    short sh = 558;
-    long l = 2822l;
-
-    float f = 5.5f;
-    double d = 5.5;
-
-    char c = 'p';
-
-    boolean b = true;
+    char c = 'a';
+    c++;
+    System.out.println(c); // b
   }
 }
