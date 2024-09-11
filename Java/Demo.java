813fe4d13c7d65a4128c42547c12ddef549680f0
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-11 23:02:37 +0700

    Multi dimensional array

--- Java/Demo.java
+++ Java/Demo.java
@@ -2,10 +2,21 @@ package Java;
 
 class Demo {
   public static void main(String args[]) {
-    int arr[] = { 1, 3, 2, 4 };
+    int arr[][] = new int[3][4];
 
-    for (int i = 0; i < arr.length; i++) {
-      System.out.print(arr[i]);
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 4; j++) {
+        arr[i][j] = (int)(Math.random() * 10);
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    for (int n[] : arr) {
+      for (int m : n) {
+        System.out.print(m + " ");
+      }
+      System.out.println();
     }
   }
 }
