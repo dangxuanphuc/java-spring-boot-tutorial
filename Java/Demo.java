5805b3904cb8d488410dd0ecf2e7eee2fde33063
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 00:00:46 +0700

    Static variables

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,12 +1,31 @@
 package Java;
 
+class Mobile {
+  String brand;
+  int price;
+  String network;
+  static String name;
+
+  public void show() {
+    System.out.println(brand + " : " + price + " : " + name);
+  }
+}
+
 class Demo {
   public static void main(String args[]) {
-    StringBuffer sb = new StringBuffer("phucdx");
+    Mobile m1 = new Mobile();
+    m1.brand = "Apple";
+    m1.price = 1500;
+    Mobile.name = "iPhone";
 
-    sb.append(" from Haiduong");
-    sb.insert(0, "Hello ");
+    Mobile m2 = new Mobile();
+    m2.brand = "Samsung";
+    m2.price = 1000;
+    Mobile.name = "Note3";
 
-    System.out.println(sb);
+    Mobile.name = "Phone";
+
+    m1.show();
+    m2.show();
   }
 }
