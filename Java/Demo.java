a23fffb50724472cec01f9a57a96991386df1cf6
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 00:23:41 +0700

    Static block

--- Java/Demo.java
+++ Java/Demo.java
@@ -6,6 +6,17 @@ class Mobile {
   String network;
   static String name;
 
+  static {
+    name = "Static Phone";
+    System.out.println("In static block");
+  }
+
+  public Mobile() {
+    brand = "";
+    price = 200;
+    System.out.println("In constructor");
+  }
+
   public void show() {
     System.out.println(brand + " : " + price + " : " + name);
   }
@@ -16,22 +27,24 @@ class Mobile {
 }
 
 class Demo {
-  public static void main(String args[]) {
-    Mobile m1 = new Mobile();
-    m1.brand = "Apple";
-    m1.price = 1500;
-    Mobile.name = "iPhone";
+  public static void main(String args[]) throws ClassNotFoundException {
+    Class.forName("Java.Mobile");
 
-    Mobile m2 = new Mobile();
-    m2.brand = "Samsung";
-    m2.price = 1000;
-    Mobile.name = "Note3";
+    // Mobile m1 = new Mobile();
+    // m1.brand = "Apple";
+    // m1.price = 1500;
+    // Mobile.name = "iPhone";
 
-    Mobile.name = "Phone";
+    // Mobile m2 = new Mobile();
+    // m2.brand = "Samsung";
+    // m2.price = 1000;
+    // Mobile.name = "Note3";
 
-    m1.show();
-    m2.show();
+    // Mobile.name = "Phone";
 
-    Mobile.show1(m1);
+    // m1.show();
+    // m2.show();
+
+    // Mobile.show1(m1);
   }
 }
