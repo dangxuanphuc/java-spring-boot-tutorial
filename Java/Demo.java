385aec449a35fd59c074986b3e8a06c4168f7e6a
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-11 22:47:39 +0700

    Array

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,24 +1,11 @@
 package Java;
 
-class Calculator {
-  int num = 5;
-
-  public int add(int n1, int n2) {
-    return n1 + n2;
-  }
-}
-
 class Demo {
   public static void main(String args[]) {
-    Calculator cal = new Calculator();
-    Calculator cal1 = new Calculator();
+    int arr[] = { 1, 3, 2, 4 };
 
-    int result = cal.add(10, 3);
-    System.out.println(result); // 13
-
-    cal.num = 8;
-
-    System.out.println(cal.num); // 8
-    System.out.println(cal1.num); // 5
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i]);
+    }
   }
 }
