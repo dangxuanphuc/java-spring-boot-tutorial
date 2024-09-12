ce2b8d522563587de07e5bcc0ebfd39adbf266af
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 15:49:14 +0700

    Upcasting & downcasting

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,43 +1,23 @@
 package Java;
 
-class Laptop {
-  String model;
-  int price;
-
-  public String toString() {
-    return "Laptop [model=" + model + ", price=" + price + "]";
+class A {
+  public void show1() {
+    System.out.println("In A show");
   }
+}
 
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    Laptop other = (Laptop) obj;
-    if (model == null) {
-      if (other.model != null)
-        return false;
-    } else if (!model.equals(other.model))
-      return false;
-    if (price != other.price)
-      return false;
-    return true;
+class B extends A {
+  public void show2() {
+    System.out.println("In B show");
   }
 }
 
 class Demo {
   public static void main(String args[]) {
-    Laptop obj = new Laptop();
-    obj.model = "iPhone";
-    obj.price = 1000;
+    A obj = (A) new B();
+    obj.show1();
 
-    Laptop obj1 = new Laptop();
-    obj1.model = "iPhone";
-    obj1.price = 1000;
-
-    boolean result = obj.equals(obj1);
-    System.out.println(result);
+    B obj1 = (B) obj;
+    obj1.show2();
   }
 }
