8f353bdc7cfa3bfe953fb3eb29fed6b28a386c16
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 11:20:10 +0700

    Overriding method

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,35 +1,22 @@
 package Java;
 
-class A {
-  public A() {
-    super();
-    System.out.println("In A");
-  }
-
-  public A(int n) {
-    super();
-    System.out.println("In A int");
+class Calc {
+  public int add(int n1, int n2) {
+    return n1 + n2;
   }
 }
 
-class B extends A {
-  public B() {
-    super(1);
-    System.out.println("In B");
-  }
-
-  public B(int n) {
-    this();
-    System.out.println("in B int");
+class AdvCalc extends Calc {
+  public int add(int n1, int n2) {
+    return n1 + n2 + 1;
   }
 }
 
 class Demo {
   public static void main(String args[]) {
-    B obj = new B(2);
+    AdvCalc obj = new AdvCalc();
 
-    // In A int
-    // In B
-    // In B int
+    int r1 = obj.add(3, 4);
+    System.out.println(r1);
   }
 }
