13938279ef7a418329c750ac6b940cfd62d29c4c
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 20:26:20 +0700

    Abstrac class

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,10 +1,32 @@
 package Java;
 
+abstract class Car {
+  public abstract void drive();
+
+  public abstract void fly();
+
+  public void playMusic() {
+    System.out.println("Play music");
+  }
+}
+
+abstract class Toyota extends Car {
+  public void fly() {
+    System.out.println("Flying...");
+  }
+}
+
+class UpdateToyota extends Toyota {
+  public void drive() {
+    System.out.println("Driving...");
+  }
+}
+
 class Demo {
   public static void main(String args[]) {
-    String str = "12";
-    int num = Integer.parseInt(str);
-
-    System.out.println(num);
+    Car obj = new UpdateToyota();
+    obj.drive();
+    obj.fly();
+    obj.playMusic();
   }
 }
