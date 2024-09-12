03b836bfc73b8dab576572e4399e418a676bd7da
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 15:27:28 +0700

    Override method

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,25 +1,43 @@
 package Java;
 
-class Calc {
-  public final void show() {
-    System.out.println("In Calc show");
+class Laptop {
+  String model;
+  int price;
+
+  public String toString() {
+    return "Laptop [model=" + model + ", price=" + price + "]";
   }
 
-  public void add(int a, int b) {
-    System.out.println(a + b);
-  }
-}
-
-class AdvCalc extends Calc {
-  public void show() { // cannot override final method
-    System.out.println("In AdvCalc show");
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Laptop other = (Laptop) obj;
+    if (model == null) {
+      if (other.model != null)
+        return false;
+    } else if (!model.equals(other.model))
+      return false;
+    if (price != other.price)
+      return false;
+    return true;
   }
 }
 
 class Demo {
   public static void main(String args[]) {
-    AdvCalc obj = new AdvCalc();
-    obj.show();
-    obj.add(3, 5);
+    Laptop obj = new Laptop();
+    obj.model = "iPhone";
+    obj.price = 1000;
+
+    Laptop obj1 = new Laptop();
+    obj1.model = "iPhone";
+    obj1.price = 1000;
+
+    boolean result = obj.equals(obj1);
+    System.out.println(result);
   }
 }
