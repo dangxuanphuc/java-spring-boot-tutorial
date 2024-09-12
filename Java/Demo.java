022693d79509a97752935d1bd93281739d4c6efa
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 10:24:39 +0700

    Anonymous constructor

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,51 +1,20 @@
 package Java;
 
 class Human {
-  private String name;
-  private int age;
-
   public Human() {
-    name = "Phucdx";
-    age = 24;
+    System.out.println("Object created");
   }
 
-  public Human(String name) {
-    this.name = name;
-    this.age = 22;
-  }
-
-  public Human(String name, int age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
+  public void show() {
+    System.out.println("In a show");
   }
 }
 
 class Demo {
   public static void main(String args[]) {
-    Human h = new Human();
-    Human h1 = new Human("Phucdx1");
-    Human h2 = new Human("Phucdx2", 26);
-    // h.setAge(26);
-    // h.setName("Phucdx");
+    new Human().show();
 
-    System.out.println(h.getName() + " " + h.getAge());
-    System.out.println(h1.getName() + " " + h1.getAge());
-    System.out.println(h2.getName() + " " + h2.getAge());
+    // Object created
+    // In a show
   }
 }
