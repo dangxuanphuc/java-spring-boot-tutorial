20796712931c64e3d07a040736a4e44d9fa3a45b
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 10:50:33 +0700

    Multi level inheritance

--- Java/Demo.java
+++ Java/Demo.java
@@ -12,13 +12,14 @@ class Calc {
 
 class Demo {
   public static void main(String args[]) {
-    AdvCalc ac = new AdvCalc();
+    VeryAdvCalc obj = new VeryAdvCalc();
 
-    int r1 = ac.add(4, 6);
-    int r2 = ac.sub(10, 7);
-    int r3 = ac.multi(10, 7);
-    int r4 = ac.div(10, 2);
+    int r1 = obj.add(4, 6);
+    int r2 = obj.sub(10, 7);
+    int r3 = obj.multi(10, 7);
+    int r4 = obj.div(10, 2);
+    double r5 = obj.power(2, 4);
 
-    System.out.println(r1 + " " + r2 + " " + r3 + " " + r4); // 10 3 70 5
+    System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5); // 10 3 70 5 16.0
   }
 }
