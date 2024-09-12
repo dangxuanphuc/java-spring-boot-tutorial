3dd2b10f4c43906c31c0ef7d4af026401a94b85a
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 09:41:58 +0700

    Encapsulation

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,50 +1,32 @@
 package Java;
 
-class Mobile {
-  String brand;
-  int price;
-  String network;
-  static String name;
+class Human {
+  private String name;
+  private int age;
 
-  static {
-    name = "Static Phone";
-    System.out.println("In static block");
+  public String getName() {
+    return name;
   }
 
-  public Mobile() {
-    brand = "";
-    price = 200;
-    System.out.println("In constructor");
+  public void setName(String n) {
+    name = n;
   }
 
-  public void show() {
-    System.out.println(brand + " : " + price + " : " + name);
+  public int getAge() {
+    return age;
   }
 
-  public static void show1(Mobile obj) {
-    System.out.println(obj.brand + " : " + obj.price + " : " + obj.name);
+  public void setAge(int a) {
+    age = a;
   }
 }
 
 class Demo {
-  public static void main(String args[]) throws ClassNotFoundException {
-    Class.forName("Java.Mobile");
+  public static void main(String args[]) {
+    Human h = new Human();
+    h.setAge(26);
+    h.setName("Phucdx");
 
-    // Mobile m1 = new Mobile();
-    // m1.brand = "Apple";
-    // m1.price = 1500;
-    // Mobile.name = "iPhone";
-
-    // Mobile m2 = new Mobile();
-    // m2.brand = "Samsung";
-    // m2.price = 1000;
-    // Mobile.name = "Note3";
-
-    // Mobile.name = "Phone";
-
-    // m1.show();
-    // m2.show();
-
-    // Mobile.show1(m1);
+    System.out.println(h.getName() + " " + h.getAge());
   }
 }
