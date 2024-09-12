3dece2e16a06cae8aedc729677262d5de314580f
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-12 10:39:22 +0700

    Inheritance

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,20 +1,24 @@
 package Java;
 
-class Human {
-  public Human() {
-    System.out.println("Object created");
+class Calc {
+  public int add(int n1, int n2) {
+    return n1 + n2;
   }
 
-  public void show() {
-    System.out.println("In a show");
+  public int sub(int n1, int n2) {
+    return n1 - n2;
   }
 }
 
 class Demo {
   public static void main(String args[]) {
-    new Human().show();
+    AdvCalc ac = new AdvCalc();
 
-    // Object created
-    // In a show
+    int r1 = ac.add(4, 6);
+    int r2 = ac.sub(10, 7);
+    int r3 = ac.multi(10, 7);
+    int r4 = ac.div(10, 2);
+
+    System.out.println(r1 + " " + r2 + " " + r3 + " " + r4); // 10 3 70 5
   }
 }
