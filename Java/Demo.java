e31351e2febaab3c95be3c41d479e8e29209091a
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 16:26:38 +0700

    Map

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,21 +1,15 @@
 package Java;
 
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
 
 class Demo {
   public static void main(String args[]) {
-    Set<Integer> nums = new TreeSet<Integer>();
-    nums.add(9);
-    nums.add(6);
-    nums.add(2);
-    nums.add(7);
+    Map<String, Integer> students = new HashMap<>();
+    students.put("A", 20);
+    students.put("B", 17);
+    students.put("C", 29);
 
-    Iterator<Integer> values = nums.iterator();
-
-    while(values.hasNext()) {
-      System.out.println(values.next());
-    }
+    System.out.println(students);
   }
 }
