90f8d3c62f136dcea73a2dad283afffe9c5548d2
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 16:56:29 +0700

    Comparator vs Comparable

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,15 +1,37 @@
 package Java;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Student {
+  int age;
+  String name;
+
+  public Student(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+
+  public String toString() {
+    return "Student [age=" + age + ", name=" + name + "]";
+  }
+}
 
 class Demo {
   public static void main(String args[]) {
-    Map<String, Integer> students = new HashMap<>();
-    students.put("A", 20);
-    students.put("B", 17);
-    students.put("C", 29);
+    Comparator<Student> com = (i, j) -> i.age > j.age ? 1 : -1;
 
-    System.out.println(students);
+    List<Student> students = new ArrayList<>();
+    students.add(new Student(21, "A"));
+    students.add(new Student(18, "B"));
+    students.add(new Student(29, "C"));
+
+    Collections.sort(students, com);
+
+    for (Student s : students) {
+      System.out.println(s);
+    }
   }
 }
