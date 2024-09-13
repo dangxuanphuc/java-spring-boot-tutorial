7e38dedbc67f0cf28adcf3fdff8552e0aba55184
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 11:22:24 +0700

    Ducking exceptions with throws

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,23 +1,28 @@
 package Java;
 
+class CustomException extends RuntimeException {
+  public CustomException(String string) {
+    super(string);
+  }
+}
+
+class A {
+  public void show() throws ClassNotFoundException {
+    Class.forName("Java");
+  }
+}
+
 class Demo {
+  static {
+    System.out.println("Class Loaded");
+  }
+
   public static void main(String args[]) {
-    int i = 20;
-    int j = 0;
-
+    A obj = new A();
     try {
-      j = 18 / i;
-      if (j == 0) {
-        throw new ArithmeticException("I dont want to print zero");
-      }
-    } catch (ArithmeticException e) {
-      j = 18 / 1;
-      System.out.println("Thats the default output " + e);
-    } catch (Exception e) {
-      System.out.println("Something went wrong..." + e);
+      obj.show();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
     }
-
-    System.out.println(j);
-    System.out.println("Bye");
   }
 }
