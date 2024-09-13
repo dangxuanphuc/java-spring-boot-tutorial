c5c0aa1ef248ea6534252cb6a3d73743edae6242
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 14:13:43 +0700

    Try finally keyword

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,26 +1,20 @@
 package Java;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.io.IOException;
 
 class Demo {
-  public static void main(String args[]) throws IOException {
-    System.out.print("Enter a number: ");
-
-    InputStreamReader in = new InputStreamReader(System.in);
+  public static void main(String args[]) throws NumberFormatException, IOException {
+    int num = 0;
     BufferedReader br = new BufferedReader(in);
 
-    int num = Integer.parseInt(br.readLine());
-    System.out.println(num);
-    br.close();
-
-    System.out.print("Enter an another number: ");
-
-    Scanner sc = new Scanner(System.in);
-    int num2 = Integer.parseInt(sc.nextLine());
-    System.out.println(num2);
-    sc.close();
+    try {
+      InputStreamReader in = new InputStreamReader(System.in);
+      num = Integer.parseInt(br.readLine());
+      System.out.println(num);
+    } finally {
+      br.close();
+    }
   }
 }
