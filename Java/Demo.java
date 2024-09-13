67a21c513dba04080a4e558db80fbc2f4bec5e05
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 21:03:56 +0700

    Optional class

--- Java/Demo.java
+++ Java/Demo.java
@@ -2,21 +2,17 @@ package Java;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 class Demo {
   public static <T> void main(String args[]) {
-    List<Integer> nums = Arrays.asList(1, 4, 2, 6, 7);
+    List<String> names = Arrays.asList("John", "Navin", "Emily", "Dele");
 
-    // Function<Integer, Integer> func = n -> n * 2;
+    String name = names.stream()
+        .filter(str -> str.contains("p"))
+        .findFirst()
+        .orElse("Not found");
 
-    // int result = nums.stream()
-    //     .filter(n -> n % 2 == 0)
-    //     .map(func)
-    //     .reduce(0, (c, e) -> c + e);
-
-    Stream<Integer> sortedValues = nums.stream().filter(n -> n % 2 == 0).sorted();
-
-    sortedValues.forEach(n -> System.out.println(n));
+    System.out.println(name);
   }
 }
