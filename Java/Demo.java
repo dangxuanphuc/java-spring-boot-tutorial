838ebe0d4282033d774b1639a1a8c6c2049a861f
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 14:56:23 +0700

    Thread priority and sleep

--- Java/Demo.java
+++ Java/Demo.java
@@ -2,16 +2,26 @@ package Java;
 
 class A extends Thread {
   public void run() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
       System.out.println("Hi");
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
 
 class B extends Thread {
   public void run() {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100; i++) {
       System.out.println("Hello");
+      try {
+        Thread.sleep(10);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
@@ -21,7 +31,16 @@ class Demo {
     A obj1 = new A();
     B obj2 = new B();
 
+    System.out.println(Thread.MAX_PRIORITY);
+
     obj1.start();
+
+    try {
+      Thread.sleep(5);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+
     obj2.start();
   }
 }
