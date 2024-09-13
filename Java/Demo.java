85764c3a04649ac3ce19d30a22a0eb2f77d554bf
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 20:31:47 +0700

    Map filter reduce sorted

--- Java/Demo.java
+++ Java/Demo.java
@@ -2,24 +2,21 @@ package Java;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 class Demo {
   public static <T> void main(String args[]) {
     List<Integer> nums = Arrays.asList(1, 4, 2, 6, 7);
 
-    // Stream<Integer> s1 = nums.stream();
-    // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
-    // Stream<Integer> s3 = s2.map(n -> n * 2);
-    // int result = s3.reduce(0, (c, e) -> c + e);
+    // Function<Integer, Integer> func = n -> n * 2;
 
-    int result = nums.stream()
-        .filter(n -> n % 2 == 0)
-        .map(n -> n * 2)
-        .reduce(0, (c, e) -> c + e);
+    // int result = nums.stream()
+    //     .filter(n -> n % 2 == 0)
+    //     .map(func)
+    //     .reduce(0, (c, e) -> c + e);
 
-    // s3.forEach(n -> System.out.println(n));
-    System.out.println(result);
+    Stream<Integer> sortedValues = nums.stream().filter(n -> n % 2 == 0).sorted();
+
+    sortedValues.forEach(n -> System.out.println(n));
   }
 }
