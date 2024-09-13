431913d83202be6cc22bde9199eca505c1a557af
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 21:19:00 +0700

    Constructor reference

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,14 +1,47 @@
 package Java;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+
+class Student {
+  private String name;
+  private int age;
+
+
+  public Student(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String toString() {
+    return "Student [name=" + name + ", age=" + age + "]";
+  }
+}
 
 class Demo {
   public static <T> void main(String args[]) {
     List<String> names = Arrays.asList("John", "Navin", "Emily", "Dele");
-    List<String> uNames = names.stream().map(String::toUpperCase).toList();
+    List<Student> students = new ArrayList<>();
 
-    uNames.forEach(System.out::println);
+    // students = names.stream().map(name -> new Student(name)).toList();
+    students = names.stream().map(Student::new).toList();
+
+    System.out.println(students);
   }
 }
