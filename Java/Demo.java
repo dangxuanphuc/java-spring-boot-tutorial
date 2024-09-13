509dda8e67d14467b7eaf8c0af37e75c72ae4e0d
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 14:34:08 +0700

    Multiple threads

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,20 +1,27 @@
 package Java;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+class A extends Thread {
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Hi");
+    }
+  }
+}
+
+class B extends Thread {
+  public void run() {
+    for (int i = 0; i < 10; i++) {
+      System.out.println("Hello");
+    }
+  }
+}
 
 class Demo {
-  public static void main(String args[]) throws NumberFormatException, IOException {
-    int num = 0;
-    BufferedReader br = new BufferedReader(in);
+  public static void main(String args[]) {
+    A obj1 = new A();
+    B obj2 = new B();
 
-    try {
-      InputStreamReader in = new InputStreamReader(System.in);
-      num = Integer.parseInt(br.readLine());
-      System.out.println(num);
-    } finally {
-      br.close();
-    }
+    obj1.start();
+    obj2.start();
   }
 }
