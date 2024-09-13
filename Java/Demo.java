86a13388eab315c6a406189254234b1fe92a57e7
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-13 10:22:10 +0700

    Exception

--- Java/Demo.java
+++ Java/Demo.java
@@ -1,14 +1,26 @@
 package Java;
 
-@FunctionalInterface
-interface A {
-  int add(int i, int j);
-}
-
 class Demo {
   public static void main(String args[]) {
-    A obj = (int i, int j) -> i + j;
-    int result = obj.add(5, 6);
-    System.out.println(result);
+    int i = 2;
+    int j = 0;
+    int nums[] = new int[5];
+    String str = null;
+
+    try {
+      j = 18 / i;
+      System.out.println(str.length());
+      System.out.println(nums[1]);
+      System.out.println(nums[5]);
+    } catch (ArithmeticException e) {
+      System.out.println("Cannot devide by zero");
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Stay in your limit");
+    } catch (Exception e) {
+      System.out.println("Something went wrong...");
+    }
+
+    System.out.println(j);
+    System.out.println("Bye");
   }
 }
