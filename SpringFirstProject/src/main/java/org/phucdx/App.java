a39663c29334d775467b6372b88a68db74492fb8
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-15 23:09:02 +0700

    Java Based Config

--- SpringFirstProject/src/main/java/org/phucdx/App.java
+++ SpringFirstProject/src/main/java/org/phucdx/App.java
@@ -1,14 +1,23 @@
 package org.phucdx;
 
+import org.phucdx.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main( String[] args ) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Alien obj1 = context.getBean("alien1", Alien.class);
-        System.out.println(obj1.getAge());
-        obj1.code();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
+
+
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Alien obj1 = context.getBean("alien1", Alien.class);
+//        System.out.println(obj1.getAge());
+//        obj1.code();
 
 //        Alien obj2 = context.getBean("alien1", Alien.class);
 //        System.out.println(obj2.age);
