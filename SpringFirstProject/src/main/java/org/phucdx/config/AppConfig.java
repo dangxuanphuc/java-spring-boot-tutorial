e7e691dcf6be5b5cc170e9a3e2087d0b2bdb2d6a
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-16 08:25:58 +0700

    Primary and Qualifier

--- SpringFirstProject/src/main/java/org/phucdx/config/AppConfig.java
+++ SpringFirstProject/src/main/java/org/phucdx/config/AppConfig.java
@@ -3,14 +3,17 @@ package org.phucdx.config;
 import org.phucdx.Alien;
 import org.phucdx.Computer;
 import org.phucdx.Desktop;
+import org.phucdx.Laptop;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configurable
 public class AppConfig {
     @Bean
-    public Alien alien(Computer com) {
+    public Alien alien(Computer com) { // @Qualifier("laptop")
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setCom(com);
@@ -20,7 +23,13 @@ public class AppConfig {
 //    @Bean(name = {"com1", "desktop1", "phucdx1"})
     @Bean
 //    @Scope("prototype")
+    @Primary
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
