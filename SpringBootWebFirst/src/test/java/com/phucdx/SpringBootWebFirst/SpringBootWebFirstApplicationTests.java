0177707ae8d2bd176f7d3242dc29ce9772e4db1b
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-16 21:16:59 +0700

    Init Spring boot web and create home page

---
+++ SpringBootWebFirst/src/test/java/com/phucdx/SpringBootWebFirst/SpringBootWebFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdx.SpringBootWebFirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
