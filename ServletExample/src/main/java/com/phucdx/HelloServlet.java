bbb4e084adb65e384690f28f98fdf19a6d10d75b
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-16 16:41:45 +0700

    Response to Client

--- ServletExample/src/main/java/com/phucdx/HelloServlet.java
+++ ServletExample/src/main/java/com/phucdx/HelloServlet.java
@@ -3,9 +3,16 @@ package com.phucdx;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class HelloServlet extends javax.servlet.http.HttpServlet {
-    public void service(javax.servlet.http.HttpServletRequest req, javax.servlet.http.HttpServletResponse res) {
+public class HelloServlet extends HttpServlet {
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
         System.out.println("In service");
+
+        res.setContentType("text/html");
+
+        PrintWriter pr = res.getWriter();
+        pr.println("<h2>Hello World</h2>");
     }
 }
