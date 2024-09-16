2fbc575791bcbc581cadeb7d42b8499d8d37a17a
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-16 22:02:49 +0700

    Request params

--- SpringBootWebFirst/src/main/java/com/phucdx/SpringBootWebFirst/HomeController.java
+++ SpringBootWebFirst/src/main/java/com/phucdx/SpringBootWebFirst/HomeController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -14,9 +15,7 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(HttpServletRequest request, HttpSession session) {
-        int num1 = Integer.parseInt(request.getParameter("num1"));
-        int num2 = Integer.parseInt(request.getParameter("num2"));
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session) {
         int result = num1 + num2;
 
         session.setAttribute("result", result);
