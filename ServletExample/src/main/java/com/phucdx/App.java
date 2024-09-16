4aac7183a52e2ba6802be6cf240486eecd4797e7
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-16 14:22:19 +0700

    Init Servlet project and running Tomcat

---
+++ ServletExample/src/main/java/com/phucdx/App.java
@@ -0,0 +1,13 @@
+package com.phucdx;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class App {
+    public static void main( String[] args ) throws LifecycleException {
+        System.out.println( "Hello World!" );
+        Tomcat tomcat = new Tomcat();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
