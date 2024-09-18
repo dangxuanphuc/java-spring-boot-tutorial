9e8ba9aed35781b6d92e6b018dcdf54f64b324e7
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-18 16:22:34 +0700

    Init spring data JPA Project

---
+++ spring-data-jpa-example/src/main/java/com/phucdx/spring_data_jpa_example/SpringDataJpaExampleApplication.java
@@ -0,0 +1,37 @@
+package com.phucdx.spring_data_jpa_example;
+
+import com.phucdx.spring_data_jpa_example.model.Student;
+import com.phucdx.spring_data_jpa_example.repo.StudentRepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDataJpaExampleApplication {
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringDataJpaExampleApplication.class, args);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+//		Student s1 = context.getBean(Student.class);
+//		Student s2 = context.getBean(Student.class);
+//		Student s3 = context.getBean(Student.class);
+//
+//		s1.setId(1);
+//		s1.setName("John");
+//		s1.setAge(22);
+//
+//		s2.setId(2);
+//		s2.setName("Jane");
+//		s2.setAge(27);
+//
+//		s3.setId(3);
+//		s3.setName("Mary");
+//		s3.setAge(18);
+//
+//		repo.save(s2);
+//		repo.save(s3);
+
+		System.out.println(repo.findAll());
+	}
+}
