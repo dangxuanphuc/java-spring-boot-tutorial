664b71934e9ae95664b660cab668c752a25d9245
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-18 09:27:31 +0700

    View data

--- JobApp/src/main/java/com/phucdx/JobApp/controller/JobController.java
+++ JobApp/src/main/java/com/phucdx/JobApp/controller/JobController.java
@@ -4,9 +4,12 @@ import com.phucdx.JobApp.model.JobPost;
 import com.phucdx.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -27,5 +30,12 @@ public class JobController {
     public String handleForm(JobPost jobPost) {
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model) {
+        List<JobPost> jobs = service.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
