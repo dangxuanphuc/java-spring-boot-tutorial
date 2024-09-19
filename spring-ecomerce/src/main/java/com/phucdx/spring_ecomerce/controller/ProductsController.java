ae9f1e578b90917a812d6a18bb7d5bcc297be880
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 10:02:47 +0700

    ResponseEntity

--- spring-ecomerce/src/main/java/com/phucdx/spring_ecomerce/controller/ProductsController.java
+++ spring-ecomerce/src/main/java/com/phucdx/spring_ecomerce/controller/ProductsController.java
@@ -3,6 +3,8 @@ package com.phucdx.spring_ecomerce.controller;
 import com.phucdx.spring_ecomerce.model.Product;
 import com.phucdx.spring_ecomerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +20,7 @@ public class ProductsController {
     private ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 }
