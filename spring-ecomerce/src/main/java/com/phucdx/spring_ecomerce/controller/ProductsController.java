e74968354d5d1c19fd7a47ef5067ac0c417900be
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 09:52:16 +0700

    Fetching all products from DB

--- spring-ecomerce/src/main/java/com/phucdx/spring_ecomerce/controller/ProductsController.java
+++ spring-ecomerce/src/main/java/com/phucdx/spring_ecomerce/controller/ProductsController.java
@@ -1,14 +1,24 @@
 package com.phucdx.spring_ecomerce.controller;
 
+import com.phucdx.spring_ecomerce.model.Product;
+import com.phucdx.spring_ecomerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class ProductsController {
+    @Autowired
+    private ProductService service;
+
     @GetMapping("/products")
-    public String getProducts() {
-        return "All products";
+    public List<Product> getProducts() {
+        return service.getAllProducts();
     }
 }
