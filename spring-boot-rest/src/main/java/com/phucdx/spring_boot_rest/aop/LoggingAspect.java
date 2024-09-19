78bca6cd33adf0dffb301112d2343fc15afa7d1c
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 16:37:22 +0700

    Using Spring AOP with Before Advice

---
+++ spring-boot-rest/src/main/java/com/phucdx/spring_boot_rest/aop/LoggingAspect.java
@@ -0,0 +1,18 @@
+package com.phucdx.spring_boot_rest.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+    @Before("execution(* com.phucdx.spring_boot_rest.service.JobService.*(..))")
+    public void logMethodCall() {
+        LOGGER.info("Method call");
+    }
+}
