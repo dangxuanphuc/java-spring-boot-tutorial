47a24c3f7f0a3c7baeac91404e9b6c4b561b03bf
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 21:06:09 +0700

    Default login form

--- spring-security-demo/src/main/java/com/phucdx/spring_security_demo/HelloController.java
+++ spring-security-demo/src/main/java/com/phucdx/spring_security_demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String greet() {
         return "Hello Spring Security";
     }
