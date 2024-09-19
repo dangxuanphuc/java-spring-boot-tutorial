6bfc2f4feb9304ee2784317bacc5566edcb054ba
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 20:32:40 +0700

    Init Spring security project

---
+++ spring-security-demo/src/main/java/com/phucdx/spring_security_demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.phucdx.spring_security_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String greet() {
+        return "Hello Spring Security";
+    }
+}
