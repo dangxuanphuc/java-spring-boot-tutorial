ff3fe58f0f0e3bc2386e0cc7545f2ec5ff53a04d
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 16:51:56 +0700

    After advice

--- spring-boot-rest/src/main/java/com/phucdx/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/phucdx/spring_boot_rest/aop/LoggingAspect.java
@@ -1,8 +1,7 @@
 package com.phucdx.spring_boot_rest.aop;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -12,8 +11,23 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..))")
+    @Before("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.phucdx.spring_boot_rest.service.JobService.updateJob(..))")
     public void logMethodCall(JoinPoint point) {
-        LOGGER.info("Method call " + point.getSignature().getName());
+        LOGGER.info("Method called " + point.getSignature().getName());
+    }
+
+    @After("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.phucdx.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodExecuted(JoinPoint point) {
+        LOGGER.info("Method executed " + point.getSignature().getName());
+    }
+
+    @AfterThrowing("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.phucdx.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodCrash(JoinPoint point) {
+        LOGGER.info("Method has some issues " + point.getSignature().getName());
+    }
+
+    @AfterReturning("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..)) || execution(* com.phucdx.spring_boot_rest.service.JobService.updateJob(..))")
+    public void logMethodExecutedSuccess(JoinPoint point) {
+        LOGGER.info("Method executed successfully " + point.getSignature().getName());
     }
 }
