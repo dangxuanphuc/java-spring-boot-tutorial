de546a1a390de8d3c78b6249b630a06f92cc8526
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-19 16:44:20 +0700

    Using JoinPoint AOP

--- spring-boot-rest/src/main/java/com/phucdx/spring_boot_rest/aop/LoggingAspect.java
+++ spring-boot-rest/src/main/java/com/phucdx/spring_boot_rest/aop/LoggingAspect.java
@@ -1,5 +1,6 @@
 package com.phucdx.spring_boot_rest.aop;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -11,8 +12,8 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Before("execution(* com.phucdx.spring_boot_rest.service.JobService.*(..))")
-    public void logMethodCall() {
-        LOGGER.info("Method call");
+    @Before("execution(* com.phucdx.spring_boot_rest.service.JobService.getJob(..))")
+    public void logMethodCall(JoinPoint point) {
+        LOGGER.info("Method call " + point.getSignature().getName());
     }
 }
