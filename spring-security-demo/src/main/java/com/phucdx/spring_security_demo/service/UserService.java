f5f8168c6f60f3408efd1c3f003138bd19c25933
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-20 10:32:06 +0700

    Enrypt password with Bcrypt

--- spring-security-demo/src/main/java/com/phucdx/spring_security_demo/service/UserService.java
+++ spring-security-demo/src/main/java/com/phucdx/spring_security_demo/service/UserService.java
@@ -3,14 +3,18 @@ package com.phucdx.spring_security_demo.service;
 import com.phucdx.spring_security_demo.model.User;
 import com.phucdx.spring_security_demo.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
     public User saveUser(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 }
