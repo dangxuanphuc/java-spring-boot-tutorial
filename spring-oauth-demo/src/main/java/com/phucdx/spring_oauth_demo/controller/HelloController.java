c5216d85dc0207d55c02c955fd538661e3867362
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2024-09-20 13:43:31 +0700

    Init Spring OAuth project

---
+++ spring-oauth-demo/src/main/java/com/phucdx/spring_oauth_demo/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.phucdx.spring_oauth_demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String greet() {
+        return "Hello World";
+    }
+}
